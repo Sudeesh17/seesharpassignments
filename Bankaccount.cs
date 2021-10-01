1daaf5b206e4e2aacfca15de88b418cd23ad0b6f
sudeesh <sudeeshpuli143@gmail.com>
2021-10-01 08:58:19 +0530

    first commit

---
+++ Bankaccount.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASSIGNMENT2
+{
+    class BankAccount
+    {
+        double balance;
+        public BankAccount()
+        {
+            balance = 500;
+        }
+        public BankAccount(double balance)
+        {
+            this.balance = balance;
+        }
+        public double getBalance()
+        {
+            return balance;
+        }
+        class BankTest
+        {
+            public static void Main(string[] args)
+            {
+                BankAccount b1 = new BankAccount();
+                Console.WriteLine("defaultbalance=" + b1.getBalance());
+                BankAccount b2 = new BankAccount(1000);
+                Console.WriteLine("mainbalance=" + b2.getBalance());
+            }
+        }
+
+
+
+    }
+
+}
---
+++ circle.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASSIGNMENT2
+{
+    class circle
+    {
+        double radius;
+        //const double PI = 3.143;
+        public void setRadius(double radius)
+        {
+            this.radius = radius;
+
+        }
+        public double GetRadius()
+        {
+            return radius;
+
+        }
+        public double CalcDiameter()
+        {
+            return 2 * radius;
+
+        }
+        public double Calcarea()
+        {
+            return Math.PI * radius * radius;
+
+        }
+
+        class circletest
+        {
+            public static void Main()
+            {
+                circle c1 = new circle();
+                c1.setRadius(2);
+                Console.WriteLine(c1.GetRadius());
+                Console.WriteLine(c1.CalcDiameter());
+                Console.WriteLine(c1.Calcarea());
+
+                c1.setRadius(4);
+                Console.WriteLine(c1.GetRadius());
+                Console.WriteLine(c1.CalcDiameter());
+                Console.WriteLine(c1.Calcarea());
+            }
+        }
+
+
+    }
+}
+
