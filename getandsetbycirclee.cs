801f1a0b197c56009caf521125dca739c22661bc
sudeesh puli <91420343+Sudeesh17@users.noreply.github.com>
2021-10-03 19:14:27 +0530

    Add files via upload

---
+++ getandsetbycirclee.cs
@@ -0,0 +1,45 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace ASSIGNMENT2
+//{
+//    class Circle
+//    {
+//        double radius;
+
+//        public double Radius
+//        {
+//            set { this.radius = value; }
+//            get { return this.radius; }
+//        }
+
+//        public double CalculateDiameter()
+//        {
+//            return 2 * radius;
+//        }
+
+//        public double CalculateArea()
+//        {
+//            return Math.PI * radius * radius;
+//        }
+        
+
+//    }
+//    class client
+//    {
+//        public static void Main()
+//        {
+//            Circle c = new Circle();
+
+//            c.Radius = 100;
+
+//            Console.WriteLine("radius:"+ c.Radius);
+//            Console.WriteLine("diameter:"+ c.CalculateDiameter());
+//            Console.WriteLine("area:" + c.CalculateArea());
+
+//        }
+//    }
+//}
